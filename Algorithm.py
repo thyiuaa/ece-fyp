da247292fe81126ff956aaf13e58fdc5eea6e044
Tim <thyiuaa@connect.ust.hk>
2021-01-11 19:35:08 +0800

    init algorithm from string input add mean correlation calculation use Algorithm 1 instead of Algorithm 2 for easier implementation remove redundant test.txt file

--- Algorithm.py
+++ Algorithm.py
@@ -21,15 +21,17 @@ class Algorithm:
     MIN_WINDOW_LENGTH: minimum window length that the algorithm will continue to process
     """
 
-    def __init__(self, pre_path, post_path, m_range, m_step, t_range, t_step):
+    def __init__(self, pre_path, post_path, m_range_str, m_step_str, t_range_str, t_step_str):
+        m_range = np.reshape(np.fromstring(m_range_str, dtype=float, sep=' '), [2, 2])
+        t_range = np.fromstring(t_range_str, dtype=float, sep=' ')
         self.pre_img = np.loadtxt(pre_path)
         self.post_img = np.loadtxt(post_path)
         self.m_start = np.array(-m_range) / 2
         self.m_end = np.array(m_range) / 2
-        self.m_step = np.array(m_step)
+        self.m_step = np.reshape(np.fromstring(m_step_str, dtype=float, sep=' '), [2, 2])
         self.t_start = np.array(-t_range) // 2
         self.t_end = np.array(t_range) // 2
-        self.t_step = np.array(t_step)
+        self.t_step = np.fromstring(t_step_str, dtype=float, sep=' ')
         self.psf = cf.psf()
         self.m_opt = np.empty(self.pre_img.shape)
         self.t_opt = np.empty(self.pre_img.shape)
@@ -48,13 +50,22 @@ class Algorithm:
         sqrt_of_multiple = np.sqrt(np.sum(np.square(X1)) * np.sum(np.square(X2)))
         return sum_of_multiple / sqrt_of_multiple
 
+    def mean_correlation(self, scale):
+        window_length = self.pre_img.shape[0] / pow(2, scale)
+        total_correlation = 0
+        number_windows = pow(4, scale)
+        for y in range(0, window_length, self.pre_img.shape[0]):
+            for x in range(0, window_length, self.pre_img.shape[1]):
+                total_correlation += self.c_opt[y, x]
+        return total_correlation / number_windows
+
     def get_range(self, start, end, step, offset):
         """
         :param start: m_start or t_start
         :param end: m_end or t_end
         :param step: m_step or t_step
         :param offset:
-        :return: a list of param to test for
+        :return: a list of param will be used in searching for a window
         """
         output = []
         arr = start + offset
@@ -72,37 +83,33 @@ class Algorithm:
         return output
 
     def store_opt_params(self, y, x, window_correlation, Mw, Tw):
-        if window_correlation < self.c_opt[y, x]:
-            return
+        if window_correlation < self.c_opt[y, x]: return
         self.m_opt[y, x] = Mw
         self.t_opt[y, x] = Tw
         self.c_opt[y, x] = window_correlation
 
-    def run(self, scale):
+    def run(self, scale):  # Algorithm 1 in p.440 is used
         window_length = self.pre_img.shape[0] / pow(2, scale)
-        if window_length < self.MIN_WINDOW_LENGTH:
-            return
+        if window_length < self.MIN_WINDOW_LENGTH: return
 
-        # initialization
-        # TODO:: offset should be optimal solution from previous scale for scale != 1
-        if scale == 1:
-            m_scale = self.get_range(self.m_start, self.m_end, self.m_step, np.array([[1, 0], [0, 1]]))
-            t_scale = self.get_range(self.t_start, self.t_end, self.t_step, np.array([0, 0]))
-        else:
-            m_scale = self.get_range(self.m_start, self.m_end, self.m_step, [[1, 0], [0, 1]])
-            t_scale = self.get_range(self.t_start, self.t_end, self.t_step, [[1, 0], [0, 1]])
+        # y, x are the coordinates of the top left element of that window in the image
+        for y in range(0, window_length, self.pre_img.shape[0]):
+            for x in range(0, window_length, self.pre_img.shape[1]):
+                # initialization
+                if scale == 1:
+                    m_scale = self.get_range(self.m_start, self.m_end, self.m_step, np.array([[1, 0], [0, 1]]))
+                    t_scale = self.get_range(self.t_start, self.t_end, self.t_step, np.array([0, 0]))
+                else:
+                    m_scale = self.get_range(self.m_start, self.m_end, self.m_step, self.m_opt[y, x])
+                    t_scale = self.get_range(self.t_start, self.t_end, self.t_step, self.t_opt[y, x])
 
-        # do affine warping and coupled filtering
-        # TODO:: study how to loop through all Mw and Tw
-        for Mw in m_scale:
-            processed_pre = cf.apply(self.pre_img, aw.affine_warping(self.psf, Mw, np.array([0, 0])))
-            for Tw in t_scale:
-                processed_post = cf.apply(aw.affine_warping(self.post_img, Mw, Tw), self.psf)
-                # calculate window correlation
-                for y in range(0, window_length, self.pre_img.shape[0]):
-                    for x in range(0, window_length, self.pre_img.shape[1]):
+                # do affine warping and coupled filtering
+                for Mw in m_scale:
+                    processed_pre = cf.apply(self.pre_img, aw.affine_warping(self.psf, Mw, np.array([0, 0])))
+                    for Tw in t_scale:
+                        processed_post = cf.apply(aw.affine_warping(self.post_img, Mw, Tw), self.psf)
                         window_correlation = self.window_correlation(processed_pre, processed_post, [y, x],
                                                                      window_length)
                         self.store_opt_params(y, x, window_correlation, Mw, Tw)
 
-        self.run(scale+1)
+        self.run(scale + 1)
