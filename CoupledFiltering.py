930041c7305876817795af31388a32442586ed5d
Kevin Chow <kevinchow.hin@gmail.com>
2021-02-27 17:42:49 +0800

    completed the psf model

--- CoupledFiltering.py
+++ CoupledFiltering.py
@@ -1,11 +1,16 @@
 import numpy as np
+import math 
 
-
-def psf():
+def psf(x,y):
     # TODO:: model the filter which is the point spread function  H(X)
     # 1. The PSF size is determined by the sigma and voxel size. It is much smaller than the image.
     # 2. It is easier to use Gaussian-weighted cosine function. (Coupled filtering #1 p.437, formula 7)
-    None
+    s_x= 0.6/2.35
+    s_y= 1.6/2.35
+    freq= 3.9
+    cos_function= math.cos(2*math.pi*freq*x)
+    psf_model= math.exp((-1/2)*((x^2)/(s_x^2)+(y^2)/(s_y^2)))*cos_function
+    return psf_model
 
 
 def apply(image, psf_filter):  # assume the filter is a P*P array where P is a odd number
