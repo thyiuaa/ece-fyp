b82fc97564a6939c2787d4f3c846e621009a0997
Kevin Chow <kevinchow.hin@gmail.com>
2021-03-11 15:46:27 +0800

    no message

--- CoupledFiltering.py
+++ CoupledFiltering.py
@@ -2,16 +2,15 @@ import math
 
 import numpy as np
 
-
 def psf():
-    s_x = 12  # PSF width (p.442) should be 0.6mm/2.35= 2.55e-4
-    s_y = 8  # PSF height           should be 1.6mm/2.35= 6.81e-4
+    s_x = 100  # PSF width (p.442) should be 0.6mm/2.35= 2.55e-4
+    s_y = 100  # PSF height           should be 1.6mm/2.35= 6.81e-4
     freq = 3.8961  # 2*(3M)/1540/1000 (ref #33 p.406)
     output = np.empty([s_y, s_x])
     for y in range(-s_y // 2, s_y // 2):
         for x in range(-s_x // 2, s_x // 2):
             cos_function = math.cos(2 * math.pi * freq * x)
-            output[y + s_y // 2, x + s_x // 2] = math.exp(-0.5 * ((x * x) / s_x * s_x + (y * y) / s_y * s_y)) * cos_function
+            output[y + s_y // 2, x + s_x // 2] = math.exp(-0.5 * ((x * x) / (2.55e-4 * 2.55e-4 )+ (y * y) / (6.81e-4 * 6.81e-4))) * cos_function
     return output
 
 
