2655372f2617416982bbea16ba3ca8c34d7280d0
Tim <thyiuaa@connect.ust.hk>
2021-03-14 22:37:02 +0800

    revert affine warping to previous version

--- AffineWarping.py
+++ AffineWarping.py
@@ -15,9 +15,11 @@ def affine_warping(arr, Mw, Tw):
     d = lateral strain
     """
     arr = np.flipud(arr)  # flip the array upside down so that bottom left element is the origin
-    new_height = int(np.ceil(arr.shape[1] * np.abs(Mw[1, 0]) + arr.shape[0] * np.abs(Mw[1, 1]) + np.abs(Tw[1])))
-    new_width = int(np.ceil(arr.shape[1] * np.abs(Mw[0, 0]) + arr.shape[0] * np.abs(Mw[0, 1]) + np.abs(Tw[0])))
-    new_arr = np.zeros([new_height, new_width])
+    # TODO:: algorithm bugged if new_arr shape != arr shape
+    # new_height = int(np.ceil(arr.shape[1] * np.abs(Mw[1, 0]) + arr.shape[0] * np.abs(Mw[1, 1]) + np.abs(Tw[1])))
+    # new_width = int(np.ceil(arr.shape[1] * np.abs(Mw[0, 0]) + arr.shape[0] * np.abs(Mw[0, 1]) + np.abs(Tw[0])))
+    # new_arr = np.zeros([new_height, new_width])
+    new_arr = np.zeros(arr.shape)
     for (y, x) in np.ndindex(arr.shape):
         new_y = (arr.shape[0] - 1) - (x * Mw[1, 0] + y * Mw[1, 1] + Tw[1])
         new_x = x * Mw[0, 0] + y * Mw[0, 1] + Tw[0]
