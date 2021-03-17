994ffc662fca4c529168847a93bba62454e9e98a
Tim <thyiuaa@connect.ust.hk>
2021-03-17 09:29:31 +0800

    push result to git

--- simulation/cli_simulation.py
+++ simulation/cli_simulation.py
@@ -12,7 +12,7 @@ import Core as c
 pre_img_path = "./2-D_Simulation_Data/rf_2media_0percent_FieldII.dat"
 post_img_path = "./2-D_Simulation_Data/rf_2media_5percent_FieldII.dat"
 
-ENV = "test"
+ENV = "sim3"
 if ENV == "sim1":
     m_range = "0.2 0.1 0.1 0.2"
     m_step = "0.002 0.002 0.002 0.002"
@@ -21,7 +21,12 @@ if ENV == "sim1":
 elif ENV == "sim2":
     m_range = "0.15 0.1 0.1 0.15"
     m_step = "0.015 0.001 0.01 0.015"
-    t_range = "12 4"
+    t_range = "22 4"
+    t_step = "1 1"
+elif ENV == "sim3":
+    m_range = "0.18 0.1 0.1 0.18"
+    m_step = "0.006 0.005 0.05 0.006"
+    t_range = "18 60"
     t_step = "1 1"
 else:
     m_range = "0.1 0.1 0.1 0.1"
@@ -35,7 +40,7 @@ algorithm = 1
 is_parallel = True
 total_cpu_ratio = 0.7
 threads = int(os.cpu_count() * total_cpu_ratio)
-max_scale = 3
+max_scale = 6
 
 if __name__ == "__main__":
     success = core.start_algo(algorithm, is_parallel, threads, max_scale)
